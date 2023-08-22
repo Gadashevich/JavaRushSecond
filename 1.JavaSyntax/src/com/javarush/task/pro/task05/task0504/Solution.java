08fa851844862b9c1b367e8cfee55b225cb519bf
GadashJohn <fox5b21@gmail.com>
2023-08-23 01:38:51 +0300

    add project

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0504/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.pro.task05.task0504;
+
+/* 
+Объединяем массивы
+*/
+
+import java.util.Arrays;
+
+public class Solution {
+    public static int[] firstArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    public static int[] secondArray = new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+    public static int[] resultArray;
+
+    public static void main(String[] args) {
+        resultArray = new int[firstArray.length+ secondArray.length];
+//        for (int i = 0; i < firstArray.length ; i++) {
+//            resultArray[i]= firstArray[i];
+//        }
+//        for (int i = 0; i < secondArray.length ; i++) {
+//            resultArray[firstArray.length+i] = secondArray[i];
+//        }
+    System.arraycopy(firstArray,0,resultArray,0,firstArray.length);
+    System.arraycopy(secondArray,0,resultArray,firstArray.length,secondArray.length);
+
+
+        for (int i = 0; i < resultArray.length; i++) {
+            System.out.print(resultArray[i] + ", ");
+        }
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0920/NimrodAi.java
@@ -0,0 +1,87 @@
+package com.javarush.task.pro.task09.task0920;
+
+/* 
+Абордаж
+*/
+
+import java.util.Arrays;
+
+public class NimrodAi {
+
+    public static String[] diningRoom = {
+            "button for emergency meeting",
+            "table", "table", "table", "table", "table",
+            "bench", "bench", "bench", "bench", "bench", "bench", "bench", "bench", "bench", "bench",
+            "bench", "bench", "bench", "bench", "bench", "bench", "bench", "bench", "bench", "bench"};
+    public static String[] o2 = {"plant", "plant"};
+    public static String[] medRoom = {"bed", "bed", "bed", "bed", "SCAN-MO-TRON-2000", "liquid analyser"};
+    public static String[] armoury = {"cannon launcher", "chair"};
+    public static String[] securityRoom = {"rack", "rack", "table"};
+    public static String[] reactor = {"reactor"};
+    public static String[] telecom = {"computer", "computer", "computer", "table", "chair"};
+    public static String[] warehouse = {
+            "container", "container", "container",
+            "box", "box", "box", "box", "box", "box",
+            "barrel", "barrel", "barrel", "barrel"};
+    public static String[] controlRoom = {"interactive ship map", "chair", "chair", "chair"};
+    public static String[] powerRoom = {"power lamp", "power lamp", "power lamp", "power lamp", "power lamp", "power lamp", "power lamp"};
+
+    public static void main(String[] args) {
+        if (checkPirates("diningRoom")) openFloodgates("diningRoom");
+        if (checkPirates("o2")) openFloodgates("o2");
+        if (checkPirates("medRoom")) openFloodgates("medRoom");
+        if (checkPirates("armoury")) openFloodgates("armoury");
+        if (checkPirates("securityRoom")) openFloodgates("securityRoom");
+        if (checkPirates("reactor")) openFloodgates("reactor");
+        if (checkPirates("telecom")) openFloodgates("telecom");
+        if (checkPirates("warehouse")) openFloodgates("warehouse");
+        if (checkPirates("controlRoom")) openFloodgates("controlRoom");
+        if (checkPirates("powerRoom")) openFloodgates("powerRoom");
+
+    }
+
+
+    public static boolean checkPirates(String roomName) {
+        String[] room = getRoomByName(roomName);
+        if (room == null) {
+            return false;
+        }
+
+        String[] scanResult = RoomScanner.scanRoom(roomName);
+        System.out.printf("В %s обнаружено: %s\n", roomName, Arrays.toString(scanResult));
+
+        if (scanResult.length > room.length) {
+            return true;
+        }
+
+        Arrays.sort(room);
+        for (String item : scanResult) {
+            if (Arrays.binarySearch(room, item) < 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void openFloodgates(String roomName) {
+        System.out.printf("ВНИМАНИЕ, открытие шлюзов в %s\n\n", roomName);
+        Arrays.fill(getRoomByName(roomName), null);
+    }
+
+    public static String[] getRoomByName(String roomName) {
+        return switch (roomName) {
+            case "diningRoom" -> diningRoom;
+            case "o2" -> o2;
+            case "medRoom" -> medRoom;
+            case "armoury" -> armoury;
+            case "securityRoom" -> securityRoom;
+            case "reactor" -> reactor;
+            case "telecom" -> telecom;
+            case "warehouse" -> warehouse;
+            case "controlRoom" -> controlRoom;
+            case "powerRoom" -> powerRoom;
+            default -> null;
+        };
+    }
+}
