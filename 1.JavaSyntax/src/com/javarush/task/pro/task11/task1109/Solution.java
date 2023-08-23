d192436d1692acb31654c25c758fd52d148c52c9
GadashJohn <fox5b21@gmail.com>
2023-08-23 17:28:03 +0300

    done -task1109

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1109/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task11.task1109;
+
+/* 
+Объекты внутренних и вложенных классов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+         new Outer().new Inner();
+         new Outer.Nested();
+    }
+}
