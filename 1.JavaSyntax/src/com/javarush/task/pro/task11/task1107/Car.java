d1ab08be2a3638212fde20c65d3cb145c8ca3e11
GadashJohn <fox5b21@gmail.com>
2023-08-23 17:17:20 +0300

    done -task1107

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1107/Car.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task11.task1107;
+
+/* 
+Двигатель — сердце автомобиля
+*/
+
+public class Car {
+
+    Engine engine = new Engine();
+
+    public class Engine {
+        private boolean isRunning;
+
+        public void start() {
+            isRunning = true;
+        }
+
+        public void stop() {
+            isRunning = false;
+        }
+
+    }
+
+}
