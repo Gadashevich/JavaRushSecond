c1f7436e0bed4e93c1b4f5d2312f8b62ab8f40e1
GadashJohn <fox5b21@gmail.com>
2023-08-23 15:56:19 +0300

    done -task1019

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Computer.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task10.task1020;
+
+public class Computer {
+
+    public Computer() {
+        new Monitor();
+        new Keyboard();
+        new Mouse();
+        new SystemUnit();
+    }
+
+
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Monitor.java
@@ -0,0 +1,5 @@
+package com.javarush.task.pro.task10.task1020;
+
+public class Monitor {
+
+}
