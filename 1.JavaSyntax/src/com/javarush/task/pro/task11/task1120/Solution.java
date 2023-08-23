fa29fe68aa13e0aba5300e499c652e0906f92fb7
GadashJohn <fox5b21@gmail.com>
2023-08-23 19:09:27 +0300

    done - task1120

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1120/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task11.task1120;
+
+import java.util.Scanner;
+
+/* 
+Все буквы маленькие
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String line = console.nextLine();
+        System.out.println(line.toLowerCase());
+    }
+}
