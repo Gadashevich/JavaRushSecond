0c1d7351ce9efdf642271c1df5a4146ce4c7d3a5
GadashJohn <fox5b21@gmail.com>
2023-08-23 18:33:53 +0300

    done - task1117

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1117/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task11.task1117;
+/*
+Пакет нужен?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("TestPackage");
+    }
+}
