f1d738f47dd24ea3d779a464740a837f443c99bf
GadashJohn <fox5b21@gmail.com>
2023-08-23 19:33:44 +0300

    done - task1126

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1126/Student.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task11.task1126;
+
+/* 
+Студент
+*/
+
+public class Student {
+    public String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+
+}
