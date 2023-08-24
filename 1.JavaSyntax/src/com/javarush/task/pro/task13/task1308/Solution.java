3d9614217cc41a7cbc6f26c823e6fd43f9673146
GadashJohn <fox5b21@gmail.com>
2023-08-24 18:17:21 +0300

    done - task1308

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1308/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task13.task1308;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/* 
+Изучаем методы класса Collections, часть 3
+*/
+
+public class Solution {
+
+    public static Integer min(ArrayList<Integer> list) {
+
+        return Collections.min(list);
+    }
+
+    public static Integer max(ArrayList<Integer> list) {
+
+        return Collections.max(list);
+    }
+
+    public static int frequency(ArrayList<Integer> list, Integer element) {
+
+        return Collections.frequency(list,element);
+    }
+
+    public static int binarySearch(ArrayList<Integer> list, Integer key) {
+
+        return Collections.binarySearch(list,key);
+    }
+}
