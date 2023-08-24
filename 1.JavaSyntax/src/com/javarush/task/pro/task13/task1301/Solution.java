9bfbea801a40d679784563cd82a2b65b4e62e4f2
GadashJohn <fox5b21@gmail.com>
2023-08-24 16:41:35 +0300

    done - task1301

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1301/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task13.task1301;
+
+import java.util.Collections;
+import java.util.HashSet;
+
+/* 
+Оформляем возврат
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String[] array = {"Через", "три", "года", "я", "буду", "Senior", "Java", "Developer"};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        System.out.println("___________________________________");
+
+        HashSet<String> hashSet = arrayToHashSet(array);
+        for(String s : hashSet) {
+            System.out.println(s);
+        }
+    }
+
+    public static HashSet<String> arrayToHashSet(String[] strings) {
+        HashSet<String> hashSet = new HashSet<>();
+        for (String string : strings) {
+                    hashSet.add(string);
+
+        }
+        return hashSet;
+    }
+}
