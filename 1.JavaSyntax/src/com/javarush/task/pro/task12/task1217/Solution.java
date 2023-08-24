b58932ea83a2da1e077108a4f556a7af6a4fcfab
GadashJohn <fox5b21@gmail.com>
2023-08-24 14:50:41 +0300

    done - task1217

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1217/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task12.task1217;
+
+/* 
+Назад в прошлое
+*/
+
+public class Solution {
+    private int[] intArray = new int[4];
+
+    public int[] getIntArray() {
+        return intArray;
+    }
+
+    public void setIntArray(int[] intArray) {
+        this.intArray = intArray;
+    }
+
+    public static void main(String[] args) {
+        var solution = new Solution();
+        var string = new String("Hello");
+        var stringBuilder = new StringBuilder();
+    }
+}
