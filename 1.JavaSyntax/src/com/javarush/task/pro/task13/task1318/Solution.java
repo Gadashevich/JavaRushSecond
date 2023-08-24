5b7c56cb0f802316c453492060648cfe9299a22b
GadashJohn <fox5b21@gmail.com>
2023-08-25 00:55:23 +0300

    done - task1318

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1318/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task13.task1318;
+
+/* 
+Следующий месяц, пожалуйста
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(getNextMonth(Month.JANUARY));
+        System.out.println(getNextMonth(Month.JULY));
+    }
+
+    public static Month getNextMonth(Month month) {
+        int ordinal = month == Month.DECEMBER ? 0 : month.ordinal() + 1;
+        return Month.values()[ordinal];
+    }
+}
