a78b36f9a33e199e17d38b1d67b078d3d066a604
GadashJohn <fox5b21@gmail.com>
2023-08-25 10:58:26 +0300

    done 1/2 - task1313

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1313/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task13.task1313;
+
+/* 
+StringLinkedList
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        StringLinkedList stringLinkedList = new StringLinkedList();
+        stringLinkedList.add("1");
+        stringLinkedList.add("2");
+        stringLinkedList.add("3");
+        stringLinkedList.add("4");
+        stringLinkedList.add("5");
+        stringLinkedList.add("6");
+        stringLinkedList.add("7");
+        stringLinkedList.add("8");
+        stringLinkedList.add("9");
+        stringLinkedList.printAll();
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1313/StringLinkedList.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task13.task1313;
+
+public class StringLinkedList {
+    private Node first = new Node();
+    private Node last = new Node();
+
+    public void printAll() {
+        Node currentElement = first.next;
+        while ((currentElement) != null) {
+            System.out.println(currentElement.value);
+            currentElement = currentElement.next;
+        }
+    }
+
+    public void add(String value) {
+        Node node = new Node();
+        node.value = value;
+     }
+
+    public static class Node {
+        private Node prev;
+        private String value;
+        private Node next;
+    }
+}
