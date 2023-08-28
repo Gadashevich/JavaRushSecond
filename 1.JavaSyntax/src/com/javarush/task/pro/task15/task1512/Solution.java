53a7c2fcc652189377568ab595950a2c7b61c883
GadashJohn <fox5b21@gmail.com>
2023-08-28 12:57:08 +0300

    done - task1512

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1512/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task15.task1512;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/* 
+Задом наперед
+*/
+
+public class Solution {
+    public static ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    public static PrintStream stream = new PrintStream(outputStream);
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        printSomething(scanner.nextLine());
+        String result = outputStream.toString();
+        outputStream.reset();
+
+       StringBuilder builder = new StringBuilder(result);
+        StringBuilder reverse = builder.reverse();
+        String string = reverse.toString();
+
+        printSomething(string);
+        System.out.println(outputStream);
+    }
+
+    public static void printSomething(String str) {
+        stream.print(str);
+    }
+}
