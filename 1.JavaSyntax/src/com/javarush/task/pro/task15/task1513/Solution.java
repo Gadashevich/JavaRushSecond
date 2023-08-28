3f0897cb44180180bff374295a19d099dca7687a
GadashJohn <fox5b21@gmail.com>
2023-08-28 13:00:58 +0300

    done - task1513

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1513/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task15.task1513;
+
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Зри в корень
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        Path root = Path.of(str).getRoot();
+        System.out.println(root);
+    }
+}
+
