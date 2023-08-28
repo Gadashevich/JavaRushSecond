6db3955ad9e220f576797ba5f5a169630a8d2862
GadashJohn <fox5b21@gmail.com>
2023-08-28 13:19:38 +0300

    done - task1515

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1515/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task15.task1515;
+
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Абсолютный путь
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        Path path = Path.of(str);
+        if(!path.isAbsolute()){
+            Path absolutePath = path.toAbsolutePath();
+            path = absolutePath;
+        }
+        System.out.println(path);
+
+    }
+}
+
