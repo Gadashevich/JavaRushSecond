40d17544aa7b176dcb95984c6616da65bb38599b
GadashJohn <fox5b21@gmail.com>
2023-08-28 11:30:35 +0300

    done - task1510

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1510/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task15.task1510;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Пишем байты в файл
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        byte[] bytes = args[0].getBytes();
+        try ( Scanner scanner = new Scanner(System.in)) {
+                Files.write(Path.of(scanner.nextLine()),bytes);
+        } catch (IOException e) {
+            System.out.println("Something went wrong : " + e);
+        }
+    }
+}
+
