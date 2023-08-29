798c2b348fe5e6d43acf461a0e259a384f9e61c7
GadashJohn <fox5b21@gmail.com>
2023-08-30 01:39:01 +0300

    done - task1619

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1619/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task16.task1619;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/* 
+Простой шаблон
+*/
+
+public class Solution {
+
+    static LocalDateTime localDateTime = LocalDateTime.now();
+
+    public static void main(String[] args) {
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyyг. Hч.mмин");
+        String text = dateTimeFormatter.format(localDateTime);
+        System.out.println(text);
+    }
+}
