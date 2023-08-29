ac60122d3e94813a10b68d46db583941668d18ad
GadashJohn <fox5b21@gmail.com>
2023-08-30 01:24:13 +0300

    done - task1616

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1616/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task16.task1616;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+/* 
+Тренировка временных зон
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        TreeSet<String> sortedZones = getSortedZones();
+        System.out.println(sortedZones.size());
+        System.out.println(sortedZones.first());
+        System.out.println(sortedZones.last());
+
+        System.out.println(getBeijingDateTime());
+    }
+
+    static TreeSet<String> getSortedZones() {
+        return new TreeSet<>(ZoneId.getAvailableZoneIds());
+    }
+
+    static ZonedDateTime getBeijingDateTime() {
+        ZoneId zoneId = ZoneId.of("Asia/Shanghai");
+        return ZonedDateTime.now(zoneId);
+
+    }
+}
