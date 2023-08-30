38076b3c6947d8d8c3cbf6d6d2cfe0ca0501117d
GadashJohn <fox5b21@gmail.com>
2023-08-30 18:03:42 +0300

    done - task1801

--- 1.JavaSyntax/src/com/javarush/task/pro/task17/task1717/Aircraft.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1717/Aircraft.java
@@ -1,6 +1,7 @@
 package com.javarush.task.pro.task17.task1717;
 
 public class Aircraft implements Flyable {
+    static String  x = "name";
     @Override
     public int getMaxSpeed() {
         return 1200;
--- 1.JavaSyntax/src/com/javarush/task/pro/task17/task1717/Eagle.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1717/Eagle.java
@@ -1,6 +1,14 @@
 package com.javarush.task.pro.task17.task1717;
 
-public class Eagle implements Flyable {
+public class Eagle extends Aircraft implements Flyable {
+
+    public static void main(String[] args)
+    {
+        System.out.println(x);
+        x = new String("abc");
+        System.out.println(x);
+    }
+
     @Override
     public int getMaxSpeed() {
         return 180;
