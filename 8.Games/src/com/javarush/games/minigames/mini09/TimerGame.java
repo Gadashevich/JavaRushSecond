e17d250a0a803defa8164412298f0b41bc7c5dd5
GadashJohn <fox5b21@gmail.com>
2023-09-01 15:25:29 +0300

    done - games.minigames.mini09

---
+++ 8.Games/src/com/javarush/games/minigames/mini09/TimerGame.java
@@ -0,0 +1,31 @@
+package com.javarush.games.minigames.mini09;
+
+import com.javarush.engine.cell.Color;
+import com.javarush.engine.cell.Game;
+
+/* 
+Таймер
+*/
+
+public class TimerGame extends Game {
+
+    @Override
+    public void initialize() {
+        setScreenSize(3, 3);
+        setTurnTimer(1000);
+    }
+
+    @Override
+    public void onTurn(int step) {
+        setCellNumber(1, 1, step);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                setCellColor(i, j, step % 2 == 0 ? Color.GREEN : Color.ORANGE);
+            }
+
+        }
+
+
+    }
+}
