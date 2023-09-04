db9afa6b8cc2b3642a9ff6fe32a96464d7f2b3b7
GadashJohn <fox5b21@gmail.com>
2023-09-04 22:59:05 +0300

    Java Core | done- task1206

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1206/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1206;
+
+/* 
+Выполняем перегрузку!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+     static void print(int i){
+
+     }
+     static void print(String s){
+
+     }
+}
