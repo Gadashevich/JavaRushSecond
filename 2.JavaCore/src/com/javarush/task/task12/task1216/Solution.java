b4b6a62b1b8cad0c73dd7fc80d1289644796b0d8
GadashJohn <fox5b21@gmail.com>
2023-09-05 00:36:49 +0300

    Java Core | done- task1216

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1216/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task12.task1216;
+
+/* 
+Летать охота
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //add an interface here - добавь интерфейс тут
+    public interface CanFly{
+
+        public void fly();
+
+
+        public void run();
+    };
+
+}
