2de0f7ed7efd41e218c315daf5350240c548af4f
GadashJohn <fox5b21@gmail.com>
2023-09-04 10:49:37 +0300

    Java Core | done- task1123

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1123/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task11.task1123;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair result = getMinimumMaximumPair(data);
+
+        System.out.println("The minimum is " + result.x);
+        System.out.println("The maximum is " + result.y);
+    }
+
+    public static Pair getMinimumMaximumPair(int[] inputArray) {
+        if (inputArray == null || inputArray.length == 0) {
+            return new Pair(null, null);
+        }
+
+        int min = Arrays.stream(inputArray).min().getAsInt();
+        int max = Arrays.stream(inputArray).max().getAsInt();
+
+        return new Pair(min, max);
+    }
+
+    public static class Pair {
+        public Integer x;
+        public Integer y;
+
+        public Pair(Integer x, Integer y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
