0326df0d165a435ffee51052b98a9853dff067c2
GadashJohn <fox5b21@gmail.com>
2023-09-04 23:02:24 +0300

    Java Core | done- task1207

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(Integer.MAX_VALUE);
+        print((Integer) 5);
+    }
+
+    static void print(int i) {
+
+    }
+
+    static void print(Integer i) {
+
+    }
+}
