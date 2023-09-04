61aa340856979414d58bc4a327f78b918cd6ece2
GadashJohn <fox5b21@gmail.com>
2023-09-04 23:46:52 +0300

    Java Core | done- task1208

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    static void print(int i){
+
+    }
+
+    static void print(Integer i){
+
+    }
+
+    static void print(String i){
+
+    }
+
+    static void print(Double i){
+
+    }
+
+    static void print(Float i){
+
+    }
+}
