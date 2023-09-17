c63131e3d66a0e1cac80bd28b11521a7cfef788b
GadashJohn <fox5b21@gmail.com>
2023-09-18 00:39:22 +0300

    done | task1328

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1328/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task13.task1328;
+
+/* 
+Битва роботов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Robot amigo = new Robot("Амиго");
+        Robot enemy = new Robot("Сгибальщик-02");
+
+        doMove(amigo, enemy);
+        doMove(amigo, enemy);
+        doMove(enemy, amigo);
+        doMove(amigo, enemy);
+        doMove(enemy, amigo);
+        doMove(amigo, enemy);
+        doMove(enemy, amigo);
+        doMove(amigo, enemy);
+    }
+
+    public static void doMove(Robot robotFirst, Robot robotSecond) {
+        BodyPart attacked = robotFirst.attack();
+        BodyPart defended = robotSecond.defense();
+        System.out.println(String.format("%s атаковал робота %s, атакована %s, защищена %s", robotFirst.getName(), robotSecond.getName(), attacked, defended));
+    }
+}
