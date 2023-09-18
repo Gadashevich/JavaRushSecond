b711befaa9c7977c55c781e53b0b0868c626fb1d
GadashJohn <fox5b21@gmail.com>
2023-09-18 23:03:45 +0300

    done | task1324

--- 2.JavaCore/src/com/javarush/task/task13/task1324/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1324/Solution.java
@@ -16,7 +16,7 @@ public class Solution {
         Integer getAge();
     }
 
-    public static abstract class Fox implements Animal{
+    public abstract static class Fox implements Animal{
         public String getName() {
             return "Fox";
         }
