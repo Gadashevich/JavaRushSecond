25293a4c0bce7bfd03a59f8b1abaa43b4e13f6de
GadashJohn <fox5b21@gmail.com>
2023-09-21 23:59:43 +0300

    done | task1408

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1408/MoldovanHen.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task14.task1408;
+
+public class MoldovanHen extends Hen {
+    @Override
+    int getCountOfEggsPerMonth() {
+        return 8;
+    }
+
+    @Override
+    String getDescription() {
+        return super.getDescription() + String.format(" Моя страна - %s. Я несу %d яиц в месяц.", Country.MOLDOVA, getCountOfEggsPerMonth());
+    }
+
+    }
+
