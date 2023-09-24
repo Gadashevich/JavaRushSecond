2d8e71d6aad87d73fa37f8dbc8b233bbf69f9ae1
GadashJohn <fox5b21@gmail.com>
2023-09-24 17:23:48 +0300

    done | task1419

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1419/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.task14.task1419;
+
+import javax.management.OperationsException; 
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipException;
+
+/* 
+Нашествие исключений
+*/
+
+public class Solution {
+    public static List<Exception> exceptions = new ArrayList<>();
+
+    public static void main(String[] args) {
+        initExceptions();
+
+        for (Exception exception : exceptions) {
+            System.out.println(exception);
+        }
+    }
+
+    private static void initExceptions() {   //the first exception
+        try {
+            float i = 1 / 0;
+
+        } catch (Exception e) {
+            exceptions.add(e);
+        }
+
+        exceptions.add(new ArrayIndexOutOfBoundsException());
+        exceptions.add(new IllegalArgumentException());
+        exceptions.add(new IllegalAccessException());
+        exceptions.add(new NumberFormatException());
+        exceptions.add(new ClassCastException());
+        exceptions.add(new IOException());
+        exceptions.add(new InterruptedIOException());
+        exceptions.add(new InterruptedException());
+        exceptions.add(new SecurityException());
+
+    }
+}
