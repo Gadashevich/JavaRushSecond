9b4a9cad494952bf129f6e3be6d0a835a4d27e99
GadashJohn <fox5b21@gmail.com>
2023-09-24 17:31:02 +0300

    done | task1420

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1420/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task14.task1420;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/* 
+НОД
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int first = Integer.parseInt(reader.readLine());
+        int second = Integer.parseInt(reader.readLine());
+
+        System.out.println(getNOD(first, second));
+    }
+
+    private static int getNOD(int first, int second) {
+        if (first < 1 || second < 1) {
+            throw new IllegalArgumentException();
+        }
+
+        while (first != second) {
+            if (first > second) {
+                first -= second;
+            }
+            if (second > first) {
+                second -= first;
+            }
+        }
+        return first;
+    }
+}
+
