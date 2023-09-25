bc9c6a00da63350a7f6bd3df36f9381677514cd8
GadashJohn <fox5b21@gmail.com>
2023-09-25 23:28:47 +0300

    done | task1515

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1515/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task15.task1515;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Статики-2
+*/
+
+public class Solution {
+    public static int A;
+    public static int B;
+
+    static {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try  {
+            A = Integer.parseInt(reader.readLine());
+            B = Integer.parseInt(reader.readLine());
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static final int MIN = min(A, B);
+
+    public static void main(String[] args) {
+        System.out.println(MIN);
+    }
+
+    public static int min(int a, int b) {
+        return a < b ? a : b;
+    }
+}
