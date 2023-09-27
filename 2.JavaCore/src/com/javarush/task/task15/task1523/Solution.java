cc3a2e1de4523270c52511285a0d1bac4c3edc7b
GadashJohn <fox5b21@gmail.com>
2023-09-28 01:19:56 +0300

    done | task1523

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+
+    public Solution(int a) {
+    }
+
+    private Solution(double b) {
+    }
+
+    protected Solution(String s) {
+    }
+
+    Solution(float f) {
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution{
+    protected SubSolution(int a) {
+        super(a);
+    }
+
+    public SubSolution(String s) {
+        super(s);
+    }
+
+      SubSolution(float f) {
+        super(f);
+    }
+}
