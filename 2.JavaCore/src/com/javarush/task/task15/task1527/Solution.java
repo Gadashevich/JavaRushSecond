6704cdc1b614770210ee29c3676ef18d114e0104
GadashJohn <fox5b21@gmail.com>
2023-09-28 23:48:02 +0300

    done | task1527

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task15.task1527;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Парсер реквестов
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String url = reader.readLine();
+        String paramsSubString = url.substring(url.lastIndexOf("?") + 1);
+        String[] splitParams = paramsSubString.split("&");
+        String objValue = null;
+
+        StringBuilder resultBuilder = new StringBuilder();
+        for (String splitParam : splitParams) {
+            String[] paramAndValue = splitParam.split("=");
+            resultBuilder.append(paramAndValue[0]);
+            resultBuilder.append(" ");
+
+            if (paramAndValue[0].equals("obj")) {
+                objValue = paramAndValue[1];
+            }
+        }
+
+        System.out.println(resultBuilder.toString().trim());
+
+        if (objValue != null) {
+            try {
+                alert(Double.parseDouble(objValue));
+            } catch (NumberFormatException nfe) {
+                alert(objValue);
+            }
+        }
+    }
+
+    public static void alert(double value) {
+        System.out.println("double: " + value);
+    }
+
+    public static void alert(String value) {
+        System.out.println("String: " + value);
+    }
+}
