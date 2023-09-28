87a6d22473d4c927f120c70b8aee5c9678797d0b
GadashJohn <fox5b21@gmail.com>
2023-09-28 11:58:47 +0300

    done | task1502

--- 2.JavaCore/src/com/javarush/task/task15/task1502/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1502/Solution.java
@@ -11,6 +11,18 @@ public class Solution {
 
     }
 
+    public static class Goose extends SmallAnimal {
+        public String getSize() {
+            return "Гусь маленький, " + super.getSize();
+        }
+    }
+
+    public static class Dragon extends BigAnimal {
+        public String getSize() {
+            return "Дракон большой, " + super.getSize();
+        }
+    }
+
     public static class BigAnimal {
         protected String getSize() {
             return "как динозавр";
@@ -20,20 +32,6 @@ public class Solution {
     public static class SmallAnimal {
         String getSize() {
             return "как кошка";
-        }
-    }
-
-    public static class Goose extends SmallAnimal {
-        @Override
-        public   String getSize() {
-            return "Гусь маленький, "+ super.getSize();
-        }
-    }
-
-    public static class Dragon extends BigAnimal {
-        @Override
-        public String getSize() {
-            return "Дракон большой, " +super.getSize();
         }
     }
 }
