b20a81ac62ebe6b4bab794d7393a6d76887aa256
GadashJohn <fox5b21@gmail.com>
2023-10-01 13:32:06 +0300

    done | task1619

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1619/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task16.task1619;
+
+/* 
+А без interrupt слабо?
+*/
+
+public class Solution {
+    public static boolean flag = true;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new TestThread());
+        t.start();
+        Thread.sleep(3000);
+        ourInterruptMethod();
+    }
+
+    public static void ourInterruptMethod() {
+        flag = false;
+    }
+
+    public static class TestThread implements Runnable {
+        public void run() {
+            while (flag) {
+                try {
+                    System.out.println("he-he");
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+}
