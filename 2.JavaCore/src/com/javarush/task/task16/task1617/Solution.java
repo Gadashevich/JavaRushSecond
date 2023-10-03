0a4f226aa7c0173bb63143a7a608f27b96f94ac8
GadashJohn <fox5b21@gmail.com>
2023-10-03 22:47:58 +0300

    done | task1617

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1617/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task16.task1617;
+
+/* 
+Отсчет на гонках
+*/
+
+public class Solution {
+    public static volatile int numSeconds = 4;
+
+    public static void main(String[] args) throws InterruptedException {
+        RacingClock clock = new RacingClock();
+        Thread.sleep(3500);
+        clock.interrupt();
+    }
+
+    public static class RacingClock extends Thread {
+        public RacingClock() {
+            start();
+        }
+
+        public void run() {
+            try {
+
+                while (numSeconds >= 1) {
+                    System.out.print(numSeconds + " ");
+                    Thread.sleep(1000);
+                    numSeconds--;
+                }
+
+                System.out.print("Марш!");
+
+            } catch (InterruptedException e) {
+                System.out.println("Прервано!");
+            }
+        }
+    }
+}
