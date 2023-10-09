65bb22051b1f09a7d7c37677f862bba8a094b0e7
GadashJohn <fox5b21@gmail.com>
2023-10-10 00:03:16 +0300

    done | task1706

---
+++ 2.JavaCore/src/com/javarush/task/task17/task1706/OurPresident.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task17.task1706;
+
+public class OurPresident {
+    private static OurPresident president;
+
+    private OurPresident() {
+    }
+
+    public static OurPresident getOurPresident() {
+        return president;
+    }
+
+    static {
+        synchronized(OurPresident.class) {
+            president = new OurPresident();
+        }
+
+    }
+}
