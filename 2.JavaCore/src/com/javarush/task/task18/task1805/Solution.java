4f07f69edca0acaface31a22e4df22f4675ef8e8
GadashJohn <fox5b21@gmail.com>
2023-10-15 23:08:39 +0300

    done | task1805

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1805/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task18.task1805;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Set;
+import java.util.TreeSet;
+
+/* 
+Сортировка байт
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+
+
+        Set<Integer> set = new TreeSet<>();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             FileInputStream inputStream = new FileInputStream(reader.readLine());) {
+            while (inputStream.available() > 0) {
+                int read = inputStream.read();
+                set.add(read);
+            }
+        }
+        for (Integer num : set) {
+            System.out.print(num + " ");
+        }
+    }
+}
