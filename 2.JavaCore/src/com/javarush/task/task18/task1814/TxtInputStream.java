79b4d7b0ae9ef002badc0b966257195ad8d8ef3c
Evgeniy Gadashevich <fox5b21@gmail.com>
2023-10-23 23:42:39 +0300

    done | task1814

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1814/TxtInputStream.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task18.task1814;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/* 
+UnsupportedFileName
+*/
+
+public class TxtInputStream extends FileInputStream {
+
+    public TxtInputStream(String fileName) throws IOException, UnsupportedFileNameException {
+        super(fileName);
+        if (!fileName.endsWith(".txt")) {
+            super.close();
+            throw new UnsupportedFileNameException();
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
+
