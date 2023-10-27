607b9a63ede9273cffbb6eb26bf818d021b0c6fa
Evgeniy Gadashevich <fox5b21@gmail.com>
2023-10-27 22:23:08 +0300

    done | task1824

--- 2.JavaCore/src/com/javarush/task/task18/task1824/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1824/Solution.java
@@ -9,11 +9,14 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         while (true) {
-            try (FileInputStream inputStream = new FileInputStream(reader.readLine())) {
+            String fileName = reader.readLine();
+            try (FileInputStream inputStream = new FileInputStream(fileName)) {
 
             } catch (FileNotFoundException e) {
-
+                System.out.println(fileName);
+                break;
             }
 
         }
