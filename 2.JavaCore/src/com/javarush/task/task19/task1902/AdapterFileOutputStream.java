aba3e81b6204be18678424d60637c7f7e8b8f8a4
Evgeniy Gadashevich <fox5b21@gmail.com>
2023-11-02 23:31:43 +0300

    done | task1902

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1902/AdapterFileOutputStream.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task19.task1902;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/* 
+Адаптер
+*/
+
+public class AdapterFileOutputStream implements AmigoStringWriter {
+    private FileOutputStream fileOutputStream;
+
+    public AdapterFileOutputStream(FileOutputStream stream) {
+        this.fileOutputStream = stream;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void flush() throws IOException {
+        fileOutputStream.flush() ;
+    }
+
+    @Override
+    public void writeString(String s) throws IOException {
+        fileOutputStream.write(s.getBytes());
+    }
+
+    @Override
+    public void close() throws IOException {
+        fileOutputStream.close();
+    }
+}
+
