8bf7b93aaedfca2ef65acbecd4ff22d4a60cfc87
Evgeniy Gadashevich <fox5b21@gmail.com>
2023-11-20 10:36:45 +0300

    done | task1910

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1910/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task19.task1910;
+
+import java.io.*;
+import java.util.ArrayList;
+
+/* 
+Пунктуация
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader reader = new BufferedReader(new FileReader(console.readLine()));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(console.readLine()))) {
+
+            while (reader.ready()) {
+                writer.write(reader.readLine().replaceAll("\\p{Punct}", ""));
+            }
+
+        } catch (IOException ignore) {
+
+        }
+
+    }
+}
