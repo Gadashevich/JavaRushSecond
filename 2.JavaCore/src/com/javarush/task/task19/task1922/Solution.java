a1effe9236b4198ed06c30251abedf4c460233c8
Evgeniy Gadashevich <fox5b21@gmail.com>
2023-11-24 00:56:54 +0300

    done | task1922

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task19.task1922;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Ищем нужные строки
+*/
+
+public class Solution {
+    public static List<String> words = new ArrayList<String>();
+
+    static {
+        words.add("файл");
+        words.add("вид");
+        words.add("В");
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new FileReader(console.readLine()));
+
+        while (reader.ready()) {
+            String line = reader.readLine();
+            String[] split = line.split(" ");
+
+            int count = 0;
+            for (String string : split) {
+                if (words.contains(string)) {
+                    count++;
+                }
+            }
+            if (count == 2) {
+                System.out.println(line);
+            }
+        }
+        console.close();
+        reader.close();
+
+
+    }
+}
