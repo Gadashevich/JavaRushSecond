55bc2550d2d586ae8f0bf014bc908b86d6cf514a
Evgeniy Gadashevich <fox5b21@gmail.com>
2023-11-25 15:46:20 +0300

    done | task1927

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1927/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task19.task1927;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/* 
+Контекстная реклама
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream out = System.out;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+        System.setOut(printStream);
+        testString.printSomething();
+        System.setOut(out);
+ 
+
+        String[] byteArray = byteArrayOutputStream.toString().split("\\n");
+        for (int i = 0; i < byteArray.length; i++) {
+            System.out.println(byteArray[i]);
+            if (i % 2 != 0)
+                System.out.println("JavaRush - курсы Java онлайн");
+        }
+    }
+
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("first");
+            System.out.println("second");
+            System.out.println("third");
+            System.out.println("fourth");
+            System.out.println("fifth");
+        }
+    }
+}
