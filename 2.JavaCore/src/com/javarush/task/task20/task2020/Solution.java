2534077606f4bd8529967051777ff98827fac3bc
Evgeniy Gadashevich <fox5b21@gmail.com>
2023-12-02 15:27:20 +0300

    done | task2020

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2020/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task20.task2020;
+
+import java.io.PrintStream;
+import java.io.Serializable;
+import java.util.logging.Logger;
+
+/* 
+Сериализация человека
+*/
+
+public class Solution {
+
+    public static class Person implements Serializable {
+        String firstName;
+        String lastName;
+        transient String fullName;
+        final transient String greeting = "Hello, ";
+        String country;
+        Sex sex;
+        transient PrintStream outputStream = System.out;
+        transient Logger logger;
+
+        Person(String firstName, String lastName, String country, Sex sex) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.fullName = String.format("%s, %s", lastName, firstName);
+            this.country = country;
+            this.sex = sex;
+            this.logger = Logger.getLogger(String.valueOf(Person.class));
+        }
+    }
+
+    enum Sex {
+        MALE,
+        FEMALE
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
