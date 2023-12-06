eea2c76879b5363a9934a7217d13bde505593483
Evgeniy Gadashevich <fox5b21@gmail.com>
2023-12-06 17:49:38 +0300

    done | task2011

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2011/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task20.task2011;
+
+import java.io.*;
+
+/* 
+Externalizable для апартаментов
+*/
+
+public class Solution {
+
+    public static class Apartment implements Externalizable{
+
+        private String address;
+        private int year;
+
+        /**
+         * Mandatory public no-arg constructor.
+         */
+        public Apartment() {
+            super();
+        }
+
+        public Apartment(String addr, int y) {
+            address = addr;
+            year = y;
+        }
+
+        /**
+         * Prints out the fields used for testing!
+         */
+        public String toString() {
+            return ("Address: " + address + "\n" + "Year: " + year);
+        }
+
+        @Override
+        public void writeExternal(ObjectOutput out) throws IOException {
+            out.writeObject(address);
+            out.writeInt(year);
+        }
+
+        @Override
+        public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+          address = (String) in.readObject();
+          year  = in.readInt();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
