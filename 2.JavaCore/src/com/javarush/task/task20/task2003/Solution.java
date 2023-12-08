f04a901040fe193adb18fda80c5c06a77dc7e4b6
Evgeniy Gadashevich <fox5b21@gmail.com>
2023-12-08 23:13:03 +0300

    done | task2003

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task20.task2003;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/* 
+Знакомство с properties
+*/
+
+public class Solution {
+
+    public static Map<String, String> runtimeStorage = new HashMap<>();
+
+    public static void save(OutputStream outputStream) throws Exception {
+        Properties properties = new Properties();
+
+        for (Map.Entry<String, String> entry : runtimeStorage.entrySet()) {
+            properties.setProperty(entry.getKey(), entry.getValue());
+        }
+
+        properties.store(outputStream,null);
+    }
+
+    public static void load(InputStream inputStream) throws IOException {
+        Properties properties = new Properties();
+        properties.load(inputStream);
+
+        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+            runtimeStorage.put(entry.getKey().toString(), entry.getValue().toString());
+        }
+    }
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             FileInputStream fos = new FileInputStream(reader.readLine())) {
+            load(fos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(runtimeStorage);
+    }
+}
