e110b5be4b7bc94086f8afc4c887766c708f3582
Evgeniy Gadashevich <fox5b21@gmail.com>
2023-12-10 10:27:55 +0300

    fix | task 3102

--- 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
@@ -1,11 +1,8 @@
 package com.javarush.task.task31.task3102;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
-import java.util.EnumSet;
 import java.util.List;
 
 /* 
@@ -13,14 +10,20 @@ import java.util.List;
 */
 
 public class Solution {
+
     public static List<String> getFileTree(String root) throws IOException {
         List<String> list = new ArrayList<>();
-        File file = new File(root);
-        File parentFile = file.getParentFile();
+        DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Path.of(root));
 
-        for (File listFile : parentFile.listFiles()) {
-            list.add(listFile.getName());
+        for (Path path : directoryStream) {
+            if (Files.isRegularFile(path)) {
+                list.add(path.toString());
+            } else if (Files.isDirectory(path)) {
+                getFileTree(path.toString());
+
+            }
         }
+
         return list;
 
     }
