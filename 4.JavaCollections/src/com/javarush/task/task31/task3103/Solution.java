19e8dc256394538e01130d3560459de74aa0da43
Evgeniy Gadashevich <fox5b21@gmail.com>
2023-12-10 21:44:50 +0300

    done | task task3103

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3103/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task31.task3103;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+/* 
+Своя реализация
+*/
+
+public class Solution {
+    public static byte[] readBytes(String fileName) throws IOException {
+        return Files.readAllBytes(Paths.get(fileName));
+    }
+
+    public static List<String> readLines(String fileName) throws IOException {
+        return Files.readAllLines(Paths.get(fileName));
+    }
+
+    public static void writeBytes(String fileName, byte[] bytes) throws IOException {
+            Files.write(Paths.get(fileName),bytes);
+    }
+
+    public static void copy(String resourceFileName, String destinationFileName) throws IOException {
+            Files.copy(Paths.get(resourceFileName),Paths.get(destinationFileName));
+    }
+}
