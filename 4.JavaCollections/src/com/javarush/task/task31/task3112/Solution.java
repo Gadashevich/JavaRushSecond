9a7ff81cbd75f58eed03738f5ede542243e4dca8
Evgeniy Gadashevich <fox5b21@gmail.com>
2023-12-12 00:03:39 +0300

    done | task task3112

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3112/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task31.task3112;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+/* 
+Загрузчик файлов
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        Path passwords = downloadFile("https://javarush.ru/testdata/secretPasswords.txt", Paths.get("D:/MyDownloads"));
+
+        for (String line : Files.readAllLines(passwords)) {
+            System.out.println(line);
+        }
+    }
+
+    public static Path downloadFile(String urlString, Path downloadDirectory) throws IOException {
+        String fileName = urlString.split("/")[urlString.split("/").length - 1];
+        Path downloadPath = downloadDirectory.resolve(fileName);
+
+        URL url = new URL(urlString);
+        InputStream inputStream = url.openStream();
+
+        Path tempFile = Files.createTempFile(null, null);
+        Files.copy(inputStream, tempFile, StandardCopyOption.REPLACE_EXISTING);
+
+        Files.move(tempFile, downloadPath);
+        return downloadPath;
+    }
+}
